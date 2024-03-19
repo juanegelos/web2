ac3a77df55e028a9e3e6379d92a59cd52d9d95e2
juanegelos <juan_gelos@yahoo.com>
2024-03-19 11:32:01 -0300

    commit inicial

---
+++ web1.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import Functions1
+
+todos = Functions1.leer_archivo()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    Functions1.escribir_archivo(todos)
+
+st.title(":blue[Mi lista de tareas] :sunglasses:")
+st.subheader("Esta es mi lista de tareas pendientes", divider=True)
+st.write(":red[Las voy a hacer cuando me de la gana]")
+
+for index, todo in enumerate(todos):
+    check_box = st.checkbox(todo, key=todo)
+    if check_box:
+        todos.pop(index)
+        Functions1.escribir_archivo(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+ 
+st.text_input(label="", placeholder="Agregue una tarea...", on_change=add_todo, 
+              key="new_todo")
+
